141cdf91506df735a640b44f15c8410f25340b70
SeptemberHX <september_hx@outlook.com>
2022-05-05 16:03:26 +0800

    add: render with css

--- src/todoRender/conferenceStyleRender.ts
+++ src/todoRender/conferenceStyleRender.ts
@@ -6,30 +6,25 @@ export function conferenceStyleRender(markdownIt, _options) {
     markdownIt.renderer.rules.text = function (tokens, idx, options, env, self) {
         if (tokens.length >= 3 && tokens[0].type === "checkbox_wrapper_open"
             && tokens[tokens.length - 1].type === "checkbox_wrapper_close") {
-            console.log(markdownIt.renderer.rules);
-            console.log(tokens, idx);
-            const token = tokens[idx];
             let result = defaultRender(tokens, idx, options, env, self);
 
             // todo: optimize the implementation. Or each string needs to be visited for 4x times
             let assignee = result.match(/@\S*/);
             if (assignee) {
-                result = result.replace(assignee[0], `[${assignee[0]}]`);
+                result = result.replace(assignee[0], `<span class="inline-todo inline-todo-assignee">${assignee[0]}</span>`);
             }
 
-            let tags = result.match(/\+\S*/);
+            let tags = result.matchAll(/\+\S*/g);
             if (tags) {
                 for (let tag of tags) {
-                    result = result.replace(tag, `<${tag}>`);
+                    result = result.replace(tag, `<span class="inline-todo inline-todo-tag tag tag-right">${tag}</span>`);
                 }
             }
 
             let date = result.match(/\/\/\S*/);
             if (date) {
-                result = result.replace(date[0], `{${date[0]}}`);
+                result = result.replace(date[0], `<span class="inline-todo inline-todo-date">${date[0].substr(2)}</span>`);
             }
-
-            console.log(result);
 
             return result
         } else {
