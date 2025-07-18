c1dc571903e95748a81f63e82fa9b10bc3864dfb
CalebJohn <git@calebjohn.ca>
2025-07-17 20:56:21 -0600

    Temporarily disable ical generation
    This was preventing the plugin from running on mobile
    (good chance it's related to this
    https://github.com/adamgibbons/ics/issues/193)
    
    I also want to somewhat conclude that experiment, it's not very elegant
    having to use a proxy server, and it's not accessible to non
    joplin-cloud users. I learned recently that apparently google calendar
    and apple calendar support caldav, so a better calendar integration
    would probably use caldav to talk to people calendars directly.

--- src/summary.ts
+++ src/summary.ts
@@ -2,7 +2,7 @@ import joplin from 'api';
 import { Settings, Summary } from './types';
 import { summaries } from './settings_tables';
 import { insertNewSummary, filterSummaryCategories } from './summary_note';
-import { icalBlock } from './ical';
+// import { icalBlock } from './ical';
 
 export async function update_summary(summary_map: Summary, settings: Settings, summary_id: string, old_body: string) {
 	let bodyFunc = summaries[settings.summary_type].func;
@@ -12,9 +12,9 @@ export async function update_summary(summary_map: Summary, settings: Settings, s
 
 	let summaryBody = await bodyFunc(filtered_map, settings);
 
-	if (settings.add_ical_block) {
-		summaryBody += icalBlock(filtered_map, settings);
-	}
+	// if (settings.add_ical_block) {
+	// 	summaryBody += icalBlock(filtered_map, settings);
+	// }
 
 	await setSummaryBody(summaryBody, summary_id, old_body, settings);
 }
@@ -25,11 +25,11 @@ async function setSummaryBody(summaryBody: string, summary_id: string, old_body:
 	// Only update the note if it actually changed...
 	if (old_body === body) { return; }
 	
-	if (settings.add_ical_block) {
-		// UIDs in the ical block change with each generation, so need to compare without them
-		// TODO: When I make the UIDs stable, this can be removed
-		if (old_body.replace(/```ical[\s\S]*```/, '') === body.replace(/```ical[\s\S]*```/, '')) { return; }
-	}
+	// if (settings.add_ical_block) {
+	// 	// UIDs in the ical block change with each generation, so need to compare without them
+	// 	// TODO: When I make the UIDs stable, this can be removed
+	// 	if (old_body.replace(/```ical[\s\S]*```/, '') === body.replace(/```ical[\s\S]*```/, '')) { return; }
+	// }
 
 	// https://github.com/laurent22/joplin/issues/5955
 	const currentNote = await joplin.workspace.selectedNote();
